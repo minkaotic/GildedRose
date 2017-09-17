0f3611c05345e2e4a499f0b2ed64bffc36b5401d
minkaotic <mia.filisch@7digital.com>
2017-09-17 17:54:18 +0100

    Simplify calculations for updating quality

--- src/GildedRose.Console/GuildedRoseApp.cs
+++ src/GildedRose.Console/GuildedRoseApp.cs
@@ -50,29 +50,27 @@ namespace GildedRose.Console
                         }
                     }
                 }
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                if (Items[i].SellIn < 0)
+                Items[i].SellIn -= 1;
+                if (Items[i].SellIn >= 0) continue;
+                if (Items[i].Name != "Aged Brie")
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Quality > 0)
                         {
-                            if (Items[i].Quality > 0)
-                            {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality -= 1;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        Items[i].Quality = 0;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality += 1;
                     }
                 }
             }
