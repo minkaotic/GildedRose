3839f99582a1a00d7c93512afd4574916f6c1f2c
minkaotic <mia.filisch@7digital.com>
2017-09-17 17:40:24 +0100

    Extract first method and improve legibility

--- src/GildedRose.Console/GuildedRoseApp.cs
+++ src/GildedRose.Console/GuildedRoseApp.cs
@@ -18,35 +18,31 @@ namespace GildedRose.Console
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != "Aged Brie" &&
+                    Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" &&
+                    Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    UpdateQualityOfDegradingItems(i);
                 }
                 else
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality += 1;
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].SellIn < 11)
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
                             if (Items[i].SellIn < 6)
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
                         }
@@ -83,6 +79,14 @@ namespace GildedRose.Console
                         }
                     }
                 }
+            }
+        }
+
+        private void UpdateQualityOfDegradingItems(int i)
+        {
+            if (Items[i].Quality > 0)
+            {
+                Items[i].Quality -= 1;
             }
         }
     }
