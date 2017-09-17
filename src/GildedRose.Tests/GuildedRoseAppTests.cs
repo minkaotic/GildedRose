c24b4e62f2601d8224a764585cfd3cd2b05fd53a
minkaotic <mia.filisch@7digital.com>
2017-09-17 11:13:07 +0100

    Ensure all items are updated correctly after day 1

--- src/GildedRose.Tests/GuildedRoseAppTests.cs
+++ src/GildedRose.Tests/GuildedRoseAppTests.cs
@@ -1,22 +1,65 @@
+using System.Linq;
+using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
 {
-    //TODO:
-    //Once the sell by date has passed, Quality degrades twice as fast
-    //The Quality of an item is never negative
-    //"Aged Brie" actually increases in Quality the older it gets
-    //The Quality of an item is never more than 50 (however "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.)
-    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-    //"Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
-
     public class GuildedRoseAppTests
     {
-        [Test]
-        public void TestTheTruth()
+        private Item _dexterityVest;
+        private Item _agedBrie;
+        private Item _mongooseElexir;
+        private Item _sulfuras;
+        private Item _backstagePasses;
+        private Item _conjuredManaCake;
+        private GuildedRoseApp _app;
+
+        [SetUp]
+        public void Setup()
         {
-            
-            Assert.True(true);
+            _app = new GuildedRoseApp();
+            _dexterityVest = _app.Items.Single(x => x.Name == "+5 Dexterity Vest");
+            _agedBrie = _app.Items.Single(x => x.Name == "Aged Brie");
+            _mongooseElexir = _app.Items.Single(x => x.Name == "Elixir of the Mongoose");
+            _sulfuras = _app.Items.Single(x => x.Name == "Sulfuras, Hand of Ragnaros");
+            _backstagePasses = _app.Items.Single(x => x.Name == "Backstage passes to a TAFKAL80ETC concert");
+            _conjuredManaCake = _app.Items.Single(x => x.Name == "Conjured Mana Cake");
         }
+
+        [Test]
+        public void SellIn_and_quality_of_each_item_are_updated_correctly_after_one_day()
+        {
+            _app.UpdateQuality();
+
+            Assert.That(_dexterityVest.Quality, Is.EqualTo(19), "Dexterity Vest's Quality should degrade by 1");
+            Assert.That(_dexterityVest.SellIn, Is.EqualTo(9),   "Dexterity Vest's SellIn should degrade by 1");
+
+            Assert.That(_agedBrie.Quality, Is.EqualTo(1), "Aged Brie's Quality should increase by 1");
+            Assert.That(_agedBrie.SellIn, Is.EqualTo(1),  "Aged Brie's SellIn should degrade by 1");
+
+            Assert.That(_mongooseElexir.Quality, Is.EqualTo(6), "Elixir of the Mongoose's Quality should degrade by 1");
+            Assert.That(_mongooseElexir.SellIn, Is.EqualTo(4),  "Elixir of the Mongoose's SellIn should degrade by 1");
+
+            Assert.That(_sulfuras.Quality, Is.EqualTo(80), "Sulfuras's Quality never degrades");
+            Assert.That(_sulfuras.SellIn, Is.EqualTo(0),   "Sulfuras's SellIn should remain at 0");
+
+            Assert.That(_backstagePasses.Quality, Is.EqualTo(21), "Backstage Passes' Quality should increase by 1");
+            Assert.That(_backstagePasses.SellIn, Is.EqualTo(14),  "Backstage Passes' SellIn should decrease by 1");
+
+            Assert.That(_conjuredManaCake.Quality, Is.EqualTo(5), "Conjured Mana Cake' Quality should decrease by 1");
+            Assert.That(_conjuredManaCake.SellIn, Is.EqualTo(2),  "Conjured Mana Cake' SellIn should decrease by 1");
+        }
+
+        /*TODO:
+        (1) Once the sell by date has passed, Quality degrades twice as fast
+        (2) The Quality of an item is never negative
+        (3) "Aged Brie" actually increases in Quality the older it gets
+        (4) The Quality of an item is never more than 50 (except Sulfuras)
+        (5) "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+        (6) "Backstage passes", like aged brie, increases in Quality as it's SellIn value
+            approaches; Quality increases by 2 when there are 10 days or less and by 3 when 
+            there are 5 days or less but Quality drops to 0 after the concert
+        */
     }
 }
+ 
